d27abca22fb5031c4f523c08b3cfd8010df5793d
Barney Fabian <bafa@tutanota.de>
2018-02-24 20:55:51 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,57 +1,57 @@
 class Person
-
-  attr_reader :name, :happiness, :hygiene
-  attr_accessor :bank_account
-
+  
+  attr_reader :name
+  attr_accessor :bank_account, :happiness, :hygiene
+  
   def initialize(name)
     @name = name
     @bank_account = 25
     @happiness = 8
     @hygiene = 8
   end
-
-  def happiness=(happiness)
-    @happiness = happiness
-    @happiness = 10 if @happiness > 10
-    @happiness = 0 if @happiness < 0
-  end
-
+  
   def hygiene=(hygiene)
     @hygiene = hygiene
     @hygiene = 10 if @hygiene > 10
     @hygiene = 0 if @hygiene < 0
   end
-
+  
+  def happiness=(happiness)
+    @happiness = happiness
+    @happiness = 10 if @happiness > 10
+    @happiness = 0 if @happiness < 0
+  end
+  
   def clean?
-    @hygiene > 7 ? true : false
+    self.hygiene > 7 ? true : false
   end
-
+  
   def happy?
-    @happiness > 7 ? true : false
+    self.happiness > 7 ? true : false
   end
-
+  
   def get_paid(salary)
     @bank_account += salary
     "all about the benjamins"
   end
-
+  
   def take_bath
     self.hygiene += 4
     "♪ Rub-a-dub just relaxing in the tub ♫"
   end
-
+  
   def work_out
-    self.happiness += + 2
+    self.happiness += 2
     self.hygiene -= 3
     "♪ another one bites the dust ♫"
   end
-
+  
   def call_friend(friend)
     self.happiness += 3
     friend.happiness += 3
-    "Hi #{friend.name}! It's #{@name}. How are you?"
+    "Hi #{friend.name}! It's #{self.name}. How are you?"
   end
-
+  
   def start_conversation(person, topic)
     if topic == "politics"
       self.happiness -= 2
@@ -65,5 +65,5 @@ class Person
       "blah blah blah blah blah"
     end
   end
-
+      
 end
